20be9c0b09e77586ba5cffa56b69f1c56441901f
Patricia <patysiq@yahoo.com.br>
2021-05-13 09:26:27 -0300

    Initial Commit

---
+++ CatCycle/CatCycleApp.swift
@@ -0,0 +1,20 @@
+//
+//  CatCycleApp.swift
+//  CatCycle
+//
+//  Created by PATRICIA S SIQUEIRA on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CatCycleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
