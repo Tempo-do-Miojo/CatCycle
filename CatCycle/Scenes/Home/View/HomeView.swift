bf944e3568747f093084e14dc48b4235923872d6
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-05-21 10:16:17 -0300

    Add componentization on HomeView

--- CatCycle/Scenes/Home/View/HomeView.swift
+++ CatCycle/Scenes/Home/View/HomeView.swift
@@ -14,34 +14,47 @@ struct HomeView: View {
             ZStack {
                 Color.ccGray3.edgesIgnoringSafeArea(.all)
                 VStack {
-                    HStack(alignment: .center) {
-                        VStack(alignment: .leading) {
-                            Text("Olá").font(.ccParagraph1).foregroundColor(Color.ccGray1)
-                            Text("Cris").font(.ccLargeTitle).foregroundColor(Color.ccGray1).bold()
-                        }
-                        .padding(.leading, 20)
+                    HStack(alignment: .top) {
+                        greetingComponent
                         Spacer(minLength: 10)
-                            Button(action: {
-                                self.showingCalendarView = true
-                            }, label: {
-                                Image("Calendar_active")
-                                    .scaledToFit()
-                            })
-                            .padding(.trailing, 20)
+                        calendarButtonComponent
                     }
-                    .padding(.top, 25)
-                    ZStack {
-                        CircleView(colorSet: ColorSets(color1: Color.ccPrimaryPurple, color2: Color.ccPrimaryPurple, color3: Color.ccPrimaryPurple, color4: Color.ccGray3), frameCycle: FrameSets(cycleX: 260, cycleY: 260), dayNow: dayNow)
-                        PeriodView(period: PeriodViewModel(fromCycle: 0.1, toCycle: 0.2, angleCycle: 0), colorTracker: Color.ccRed)
-                        PeriodView(period: PeriodViewModel(fromCycle: 0.1, toCycle: 0.15, angleCycle: 180), colorTracker: Color.ccBlue)
-                        DayNow()
-                    }
+                    .padding(.horizontal, 24)
+                    .padding(.top, 24)
+                    completePeriodComponent
                     Information(titles: ["Day"], infos: [.init(id: 0, iconName: "Bleeding_Medium", text: "Medium", type: .bleeding), .init(id: 1, iconName: "Symptoms_Cramps", text: "Medium", type: .symptoms)])
                         .padding(.horizontal,24)
                     Spacer()
                 }
             }
         }
+
+    var greetingComponent: some View {
+        VStack(alignment: .leading) {
+            Text("Olá").font(.ccParagraph1).foregroundColor(Color.ccGray1)
+            Text("Cris").font(.ccLargeTitle).foregroundColor(Color.ccGray1).bold()
+        }
+    }
+
+    var calendarButtonComponent: some View {
+        Button(action: {
+            self.showingCalendarView = true
+        }, label: {
+            Image("Calendar_active")
+                .scaledToFit()
+        })
+    }
+
+    var completePeriodComponent: some View {
+        ZStack {
+            CircleView(colorSet: ColorSets(color1: Color.ccPrimaryPurple, color2: Color.ccPrimaryPurple, color3: Color.ccPrimaryPurple, color4: Color.ccGray3), frameCycle: FrameSets(cycleX: 260, cycleY: 260), dayNow: dayNow)
+            PeriodView(period: PeriodViewModel(fromCycle: 0.1, toCycle: 0.2, angleCycle: 0), colorTracker: Color.ccRed)
+            PeriodView(period: PeriodViewModel(fromCycle: 0.1, toCycle: 0.15, angleCycle: 180), colorTracker: Color.ccBlue)
+            DayNow()
+        }
+
+    }
+
     func getTextFromDate(date: Date!) -> String {
         let formatter = DateFormatter()
         formatter.locale = .current
