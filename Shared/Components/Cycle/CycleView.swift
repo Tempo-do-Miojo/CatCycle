d9df9b8fd6915153eb274120db054d0116e669ed
Patricia <patysiq@yahoo.com.br>
2021-05-20 22:47:25 -0300

    delete some comments

--- Shared/Components/Cycle/CycleView.swift
+++ Shared/Components/Cycle/CycleView.swift
@@ -41,24 +41,3 @@ struct CircleView: View {
         return date == nil ? "?" : formatter.string(from: date)
     }
 }
-
-//Liquid()
-//    .frame(width: 260, height: 260)
-//    .foregroundColor(colorSet.color1)
-//    .opacity(0.3)
-//
-//Liquid()
-//    .frame(width: 240, height: 240)
-//    .foregroundColor(colorSet.color2)
-//    .opacity(0.6)
-//
-//Liquid(samples: 10)
-//    .frame(width: 220, height: 220)
-//    .foregroundColor(colorSet.color3)
-//Circle()
-//    .frame(width: 180, height: 180)
-//    .foregroundColor(colorSet.color4)
-//VStack {
-//    Text("Day").font(.ccTitle1).foregroundColor(Color.ccGray1)
-//    Text("\(getTextFromDate(date: dayNow))").font(.ccLargeTitle).foregroundColor(Color.ccGray1)
-//}
