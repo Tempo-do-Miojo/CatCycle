d9f4fa4a81f77bbb003e7e3cf9d4c55bd3ea89b6
Patricia <patysiq@yahoo.com.br>
2021-05-26 11:30:37 -0300

    add HomeModel

--- CatCycle/Scenes/Home/ViewModel/HomeViewModel.swift
+++ CatCycle/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -13,11 +13,9 @@ struct HomeViewModel {
         guard let user = CoreDataManager.fetchUser() else {return ""}
         return user.name ?? ""
     }
-    func getBleendingPeriodInitial() -> CGFloat {
-        guard let user = CoreDataManager.fetchUser(),
-              let dateInitialBleending = user.initialDate else {return 0.0}
-        let dateNumber:CGFloat = CGFloat(DayViewModel.getNumberFromDate(date: dateInitialBleending))
-        let initialDate: CGFloat = dateNumber / CGFloat(DayViewModel.getNumberOfDaysInMonth(date: dateInitialBleending))
+    func getBleendingPeriodInitial(date: Date!) -> CGFloat {
+        let dateNumber:CGFloat = CGFloat(DayViewModel.getNumberFromDate(date: getFirstBleeding(date: date)))
+        let initialDate: CGFloat = dateNumber / CGFloat(DayViewModel.getNumberOfDaysInMonth(date: getFirstBleeding(date: date)))
         return initialDate
     }
     func getBleendingPeriodFinish() -> CGFloat {
@@ -26,5 +24,12 @@ struct HomeViewModel {
         let periodLenght = CGFloat(user.periodLength + Int16(DayViewModel.getNumberFromDate(date: dateInitialBleending)))
         let finishDate: CGFloat = periodLenght / CGFloat(DayViewModel.getNumberOfDaysInMonth(date: dateInitialBleending))
         return finishDate
+    }
+    
+    func getFirstBleeding(date: Date!) -> Date {
+        guard let user = CoreDataManager.fetchUser(),
+              let dateInitialBleending = user.initialDate else {return date}
+        let dayBleeding = CoreDataManager.fetchDay(in: DayViewModel.getNumberFromMonth(date: date), year: DayViewModel.getNumberFromYear(date: date))
+        return dayBleeding.first?.date ?? dateInitialBleending
     }
 }
